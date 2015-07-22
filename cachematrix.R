a6598ce155abf0dbc6b6a4d6db76f2ad41bb1fa4
Vanessa McBride <vanessa@ast.uct.ac.za>
2015-07-22 21:18:51 +0200

    cachematrix.R completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix function comprises two related functions which store the matrix
+## and its inverse in the cache
 
-## Write a short comment describing this function
+cachematrix <- function (x = matrix()){
+  inv <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function puts a new matrix, z, in the cache and nulls the current inverse
+  makeCacheMatrix <- function(z) {
+    x <<- z
+    inv <<- NULL
+  }
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## This function solves for the inverse of the matrix, x, and keeps the result in cache.
+## It will only recalculate the inverse if a new matrix has been read in.
+  cacheSolve <- function() {
         ## Return a matrix that is the inverse of 'x'
+    if (!is.null(inv)){
+      return(inv)
+    } else {
+      inv <<- solve(x)
+      return(inv)
+    }
+  }
+  list(makeCacheMatrix=makeCacheMatrix, cacheSolve=cacheSolve)
 }
